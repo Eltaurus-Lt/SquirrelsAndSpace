70c7886b8c9ac730127c40ef75902759f38a7071
Eltaurus <eltaurus@inbox.lt>
2024-12-27 07:29:44 +0600

    dummy plot

--- scripts/refs.js
+++ scripts/refs.js
@@ -3,7 +3,7 @@ const fig_ids = [...document.querySelectorAll('.figure-container')].map(L => L.i
 [...document.getElementsByTagName('figref')].forEach(ref => {
   const refId = ref.getAttribute('ref');
   const counterValue = fig_ids.indexOf(refId)+1;
-console.log(counterValue);
+
   if (counterValue) {
     ref.innerHTML = `<a href="#${refId}">${counterValue}</a>`;
   }
