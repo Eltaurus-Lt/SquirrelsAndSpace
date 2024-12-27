a4ea9672d0d661db23ef8903f328fe617308282b
Eltaurus <eltaurus@inbox.lt>
2024-12-27 09:43:20 +0600

    flow plot

--- scripts/pLTs.js
+++ scripts/pLTs.js
@@ -1,16 +1,21 @@
-const plotStyle = getComputedStyle(document.documentElement);
-function cssFloat(variable) {
-  return parseFloat(plotStyle.getPropertyValue(variable).trim());
-}
-const plotRangeLeft = cssFloat('--PlotRange-left');
-const plotRangeRight = cssFloat('--PlotRange-right');
-const plotRangeTop = cssFloat('--PlotRange-top');
-const plotRangeBottom = cssFloat('--PlotRange-bottom');
-const scaleRatio = cssFloat('font-size')/cssFloat('line-height');
+setTimeout(() => {
 
-const viewBoxStr = `${plotRangeLeft} ${plotRangeBottom / scaleRatio} ${plotRangeRight - plotRangeLeft} ${(plotRangeTop - plotRangeBottom) / scaleRatio}`;
+  const plotStyle = getComputedStyle(document.documentElement);
+  function cssFloat(variable) {
+    return parseFloat(plotStyle.getPropertyValue(variable).trim());
+  }
+  const plotRangeLeft = cssFloat('--PlotRange-left');
+  const plotRangeRight = cssFloat('--PlotRange-right');
+  const plotRangeTop = cssFloat('--PlotRange-top');
+  const plotRangeBottom = cssFloat('--PlotRange-bottom');
+  const scaleRatio = cssFloat('font-size')/cssFloat('line-height');
+
+  const viewBoxStr = `${plotRangeLeft} ${plotRangeBottom / scaleRatio} ${plotRangeRight - plotRangeLeft} ${(plotRangeTop - plotRangeBottom) / scaleRatio}`;
 
 const plotSVGs = document.querySelectorAll('svg.plot');
-plotSVGs.forEach(svg => svg.setAttribute('viewBox', viewBoxStr));
+  plotSVGs.forEach(svg => svg.setAttribute('viewBox', viewBoxStr));
+
+}, 10);
+
 
 
